cb77e7482ed97887290bdbd5c98c87d41d104adc
lwesy <lw@esyoil.com>
2017-12-09 16:55:52 +0100

    add routes for todos

--- todos/controller.js
+++ todos/controller.js
@@ -1,9 +1,37 @@
-const getAllTodos = (req, res) => {
+const getAll = (req, res) => {
   res.status(200).json({
-    msg: 'controller woah'
-  })
+    msg: 'GET /todos'
+  });
+};
+
+const getOne = (req, res) => {
+  res.status(200).json({
+    msg: 'GET /todos/:id'
+  });
+};
+
+const create = (req, res) => {
+  res.status(200).json({
+    msg: 'POST /todos'
+  });
+};
+
+const update = (req, res) => {
+  res.status(200).json({
+    msg: 'PUT /todos/:id'
+  });
+};
+
+const remove = (req, res) => {
+  res.status(200).json({
+    msg: 'DELETE /todos/:id'
+  });
 };
 
 module.exports = {
-  getAllTodos
+  getAll,
+  getOne,
+  create,
+  update,
+  remove
 };
--- todos/routes.js
+++ todos/routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const todosController = require('./controller');
 const router = new express.Router();
 
-router.get('/todos', todosController.getAllTodos);
+router.get('/todos', todosController.getAll);
+router.get('/todos/:id', todosController.getOne);
+router.post('/todos', todosController.create);
+router.put('/todos/:id', todosController.update);
+router.delete('/todos/:id', todosController.remove);
 
 module.exports = router;

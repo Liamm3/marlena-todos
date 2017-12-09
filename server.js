8a514e92537e01fc42426131076c2b5ac7622c89
lwesy <lw@esyoil.com>
2017-12-09 18:16:16 +0100

    add bodyParser

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('./db/mongoose');
+const bodyParser = require('body-parser')
 
 const { todosRoutes } = require('./todos')
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api', todosRoutes);
 
 app.listen(port, () => {
